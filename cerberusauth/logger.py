fa922bbd9aa991325559181c6eca7b0b3877e9d0
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-02 14:10:42 +0000

    feat(logging): Improve docstring documentation for setup_logging.

--- cerberusauth/logger.py
+++ cerberusauth/logger.py
@@ -13,7 +13,21 @@ from . import config
 def setup_logging(
     config_path=config.LOGGING_CONFIG_FILE
 ):
-    """Setup logging configuration."""
+    """
+    Setup logging configuration, to be called on initilisation of application.
+
+    This will attempt to set-up logging based on the configuration file
+    logging.yml. This configuration can be overridden by passing the path to a
+    similar YAML file as the ENV variable LOGGING_CONFIG_FILE.
+
+    Once set-up, the rest of the microservice can simply create a logging
+    instance as normal, with::
+
+        import logging
+
+        logger = logging.getLogger(__name__)
+
+    """
     if os.path.exists(config_path):
         with open(config_path, 'rt') as f:
             logging_config = yaml.safe_load(f.read())
