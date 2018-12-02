974a3b7068605ba05ece07e5068dfe322d46128c
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-02 19:42:57 +0000

    feat(models): Add SQLAlchemy models that derive from the base models.

---
+++ cerberusauth/models/sqlalchemy.py
@@ -0,0 +1,52 @@
+"""
+SQLAlchemy Models.
+"""
+
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_repr import RepresentableBase
+from sqlalchemy_utc import UtcDateTime, utcnow
+
+from . import User as BaseUser, Role as BaseRole, Permission as BasePermission
+
+
+class BaseSQLModel(RepresentableBase):
+    """Base model for SQLAlchemy."""
+    id = Column('id', Integer, primary_key=True)
+    created = Column(UtcDateTime, default=utcnow())
+    modified = Column(UtcDateTime, onupdate=utcnow())
+
+BaseSQLModel = declarative_base(cls=BaseSQLModel)
+
+
+class User(BaseSQLModel, BaseUser):
+    """User model for SQLAlchemy."""
+    __tablename__ = 'user'
+
+    email = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    fullname = Column(String(255))
+
+    __init__ = BaseUser.__init__
+
+
+class Role(BaseSQLModel, BaseRole):
+    """Role model for SQLAlchemy."""
+    __tablename__ = 'role'
+
+    name = Column(String(255), nullable=False)
+    description = Column(String(255))
+    enabled = Column(Boolean())
+
+    __init__ = BaseRole.__init__
+
+
+class Permission(BaseSQLModel, BasePermission):
+    """Permission model for SQLAlchemy."""
+    __tablename__ = 'permission'
+
+    slug = Column(String(255), nullable=False)
+    description = Column(String(255))
+    enabled = Column(Boolean())
+
+    __init__ = BasePermission.__init__
