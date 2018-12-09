d0b9a1c5db4826b66dff8ef2f47aac158127792d
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-09 19:17:26 +0000

    feat(Repository SQL adapter): Create SQL adapter for save, delete, get, and count.

---
+++ cerberusauth/repository/sqladapter.py
@@ -0,0 +1,38 @@
+"""
+Repository adapter for SQL storage.
+"""
+
+from sqlalchemy import func
+
+
+class SQLRepositoryAdapter(object):
+    """Repository adapter for SQL storage."""
+
+    def __init__(self, session):
+        """Initialise an instance."""
+        self.session = session
+
+    def save(self, aggregate_root):
+        """Save a SQLAlchemy model."""
+        self.session.add(aggregate_root)
+        return aggregate_root
+
+    def count(self, cls):
+        """Return a count of aggregate roots."""
+        return (
+            self.session
+            .query(func.count(cls.id))
+            .scalar()
+        )
+
+    def get(self, cls, aggregate_root_id):
+        """Get an aggregate root."""
+        return (
+            self.session
+            .query(cls)
+            .get(aggregate_root_id)
+        )
+
+    def delete(self, aggregate_root):
+        """Delete an aggregate root."""
+        self.session.delete(aggregate_root)
