c0b735a8e68c348c60aff81be28c0a3d7e75d9e5
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-10 23:12:59 +0000

    refactor(Configuration): Add a config variable for STORAGE_STRATEGY.

--- cerberusauth/config.py
+++ cerberusauth/config.py
@@ -13,3 +13,6 @@ _config_path = os.path.dirname(os.path.realpath(__file__))
 LOGGING_CONFIG_FILE = os.getenv(
     "LOGGING_CONFIG_FILE", os.path.join(_config_path, "logging.yml")
 )
+STORAGE_STRATEGY = os.getenv(
+    "STORAGE_STRATEGY", 'sql'
+)
