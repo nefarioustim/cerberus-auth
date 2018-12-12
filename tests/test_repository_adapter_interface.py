bfa68ed43706e805b25cfa4a53c2e21414f38f91
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-12 22:41:00 +0000

    test(RepositoryAdapterInterface): Add tests that interface methods raise NotImplementedError.

---
+++ tests/test_repository_adapter_interface.py
@@ -0,0 +1,39 @@
+"""
+Tests for RepositoryAdapter interface.
+"""
+
+from unittest.mock import Mock
+import pytest
+from cerberusauth.repository import adapter
+
+
+def test_interface_has_save():
+    """."""
+    interface = adapter.RepositoryAdapterInterface()
+
+    with pytest.raises(NotImplementedError):
+        interface.save(Mock())
+
+
+def test_interface_has_count():
+    """."""
+    interface = adapter.RepositoryAdapterInterface()
+
+    with pytest.raises(NotImplementedError):
+        interface.count()
+
+
+def test_interface_has_get():
+    """."""
+    interface = adapter.RepositoryAdapterInterface()
+
+    with pytest.raises(NotImplementedError):
+        interface.get(1234)
+
+
+def test_interface_has_delete():
+    """."""
+    interface = adapter.RepositoryAdapterInterface()
+
+    with pytest.raises(NotImplementedError):
+        interface.delete(Mock())
