9d767aaf7283bcebf7a83f41599d4f00534b6899
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-15 12:37:12 +0000

    test(Repository Tests): Add instantiation and get_aggregate_root_object tests for PermissionReposito

--- tests/test_repository_permission.py
+++ tests/test_repository_permission.py
@@ -2,6 +2,9 @@
 
 from cerberusauth.repository import permission
 from cerberusauth.repository.adapter import sql
+from cerberusauth.models import sql as sql_models
+
+from . import data_for_tests
 
 
 def test_permission_repository_factory():
@@ -11,3 +14,22 @@ def test_permission_repository_factory():
     assert repo
     assert isinstance(repo, permission.PermissionRepository)
     assert isinstance(repo.adapter, sql.SQLRepositoryAdapter)
+    assert repo.agg_root_class is sql_models.Permission
+
+
+def test_get_aggregate_root_object_returns_permission():
+    """."""
+    repo = permission.get_repository()
+
+    agg_root = repo.get_aggregate_root_object(
+        data_for_tests.get_permission()
+    )
+
+    assert agg_root
+    assert isinstance(agg_root, repo.agg_root_class)
+
+    # Assert sending it an aggregate root object also works.
+    agg_root = repo.get_aggregate_root_object(agg_root)
+
+    assert agg_root
+    assert isinstance(agg_root, repo.agg_root_class)
