bd9376199db60d18509df86d54bae63f4987cf9f
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-15 14:05:43 +0000

    feat(PermissionRepository): Add implementations to PermissionRepository and test count and get.

--- cerberusauth/repository/permission.py
+++ cerberusauth/repository/permission.py
@@ -4,41 +4,63 @@ Repository object for Permission models.
 
 import logging
 from . import adapter
+from .. import models
+from .. import config
 
 
-def get_repository(session=None, adapter_obj=None):
+def get_repository(session=None, storage_strategy=None, logger=None):
     """PermissionRepository factory."""
     return PermissionRepository(
-        adapter=adapter_obj or adapter.repository_adapter_factory(session)
+        session=session,
+        storage_strategy=storage_strategy,
+        logger=logger
     )
 
 
 class PermissionRepository(object):
     """Provide CRUD behaviour for aggregate roots."""
 
-    def __init__(self, adapter, logger=None):
-        self.adapter = adapter
+    def __init__(self, session=None, storage_strategy=None, logger=None):
+        self.storage_strategy = storage_strategy or config.STORAGE_STRATEGY
+        self.agg_root_class = models.get_permission_class(
+            storage_strategy)
+        self.adapter = adapter.repository_adapter_factory(
+            session, storage_strategy)
         self.logger = logger or logging.getLogger(__name__)
 
-    @staticmethod
-    def _prepare_aggregate_root(agg_root_class, agg_root):
+    def get_aggregate_root_object(self, agg_root):
         if isinstance(agg_root, dict):
-            agg_root = agg_root_class(**agg_root)
+            agg_root = self.agg_root_class(**agg_root)
 
         return agg_root
 
     def count(self):
         """Return a count of aggregate roots."""
-        raise NotImplementedError
+        return self.adapter.count(self.agg_root_class)
+
+    def _get(self, agg_root_id):
+        agg_root = self.adapter.get(self.agg_root_class, agg_root_id)
+        self.logger.info('Got {} with ID {}'.format(
+            self.agg_root_class.__name__, agg_root_id))
+        return agg_root
+
+    def get(self, *aggregate_root_ids):
+        """Get an aggregate root or roots."""
+        return [
+            self._get(agg_root_id) for agg_root_id in aggregate_root_ids
+        ]
 
     def save(self, *aggregate_roots):
         """Save an aggregate root or roots."""
-        raise NotImplementedError
-
-    def get(self, *aggregate_roots):
-        """Get an aggregate root or roots."""
-        raise NotImplementedError
+        return [
+            self.adapter.save(
+                self.get_aggregate_root_object(agg_root)
+            ) for agg_root in aggregate_roots
+        ]
 
     def delete(self, *aggregate_roots):
         """Delete an aggregate root or roots."""
-        raise NotImplementedError
+        for agg_root in aggregate_roots:
+            self.adapter.delete(
+                self.get_aggregate_root_object(agg_root)
+            )
--- tests/test_repository_permission.py
+++ tests/test_repository_permission.py
@@ -1,5 +1,9 @@
 """Tests for PermissionRepository."""
 
+from unittest import mock
+
+import pytest
+
 from cerberusauth.repository import permission
 from cerberusauth.repository.adapter import sql
 from cerberusauth.models import sql as sql_models
@@ -33,3 +37,44 @@ def test_get_aggregate_root_object_returns_permission():
 
     assert agg_root
     assert isinstance(agg_root, repo.agg_root_class)
+
+
+def test_count_calls_adapter():
+    """."""
+    repo = permission.get_repository()
+    repo.adapter = mock.Mock()
+
+    repo.count()
+
+    repo.adapter.count.assert_called_once()
+    repo.adapter.count.assert_called_with(repo.agg_root_class)
+
+
+def test_get(caplog):
+    """."""
+    repo = permission.get_repository()
+    repo.adapter = mock.Mock()
+
+    with caplog.at_level("INFO"):
+        repo.get(1)
+
+    repo.adapter.get.assert_called_once_with(repo.agg_root_class, 1)
+    assert 'Got {} with ID 1'.format(
+        repo.agg_root_class.__name__
+    ) in caplog.text
+
+
+def test_get_batch(caplog):
+    """."""
+    test_ids = [1, 2, 3, 4]
+    repo = permission.get_repository()
+    repo.adapter = mock.Mock()
+
+    with caplog.at_level("INFO"):
+        repo.get(*test_ids)
+
+    for test_id in test_ids:
+        repo.adapter.get.assert_any_call(repo.agg_root_class, test_id)
+        assert 'Got {} with ID {}'.format(
+            repo.agg_root_class.__name__, test_id
+        ) in caplog.text
