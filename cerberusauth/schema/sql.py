6bff64c7a0d43d2da5915822aaaffa49ae616783
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-19 16:30:21 +0000

    feat(Schema creation): Add basic schema package and a skeletal Schema object for SQL.

---
+++ cerberusauth/schema/sql.py
@@ -0,0 +1,13 @@
+"""
+Datastore schema for SQL.
+"""
+
+
+class Schema(object):
+    """Controls creation of datastore schema."""
+
+    def import_models(self):
+        """Import models to create as schema."""
+
+    def create_schema(self):
+        """Create the schema."""
