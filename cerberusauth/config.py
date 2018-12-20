4549fce7890446e893eb7eaf0edc2c1762640dae
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-20 21:59:28 +0000

    refactor(Configuration): Refactor configuration to use environment variables.

--- cerberusauth/config.py
+++ cerberusauth/config.py
@@ -13,6 +13,6 @@ _config_path = os.path.dirname(os.path.realpath(__file__))
 LOGGING_CONFIG_FILE = os.getenv(
     "LOGGING_CONFIG_FILE", os.path.join(_config_path, "logging.yml")
 )
-STORAGE_STRATEGY = os.getenv(
-    "STORAGE_STRATEGY", 'sql'
-)
+STORAGE_STRATEGY = os.getenv("STORAGE_STRATEGY", "sql")
+STORAGE_USER = os.getenv("STORAGE_USER", None)
+STORAGE_PASSWORD = os.getenv("STORAGE_PASSWORD", None)
