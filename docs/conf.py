e669227ba6a36ef5f03ad9036e954b9848f85b5f
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-30 23:02:55 +0000

    docs(Sphinx): Adjust config for Sphinx build.

--- docs/conf.py
+++ docs/conf.py
@@ -80,12 +80,18 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'nefarioustim',
+    'github_repo': 'cerberus-auth',
+    'description': 'A user authentication and authorisation microservice',
+    'travis_button': True,
+    'show_powered_by': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -95,7 +101,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
