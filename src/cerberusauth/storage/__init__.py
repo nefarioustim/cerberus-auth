e8a0752814d26e06698fc1cee087207084aed929
Tim Huegdon <tim@nefariousdesigns.co.uk>
2018-12-31 00:01:28 +0000

    feat(Storage): Provide method to test if a storage session is necessary.

--- src/cerberusauth/storage/__init__.py
+++ src/cerberusauth/storage/__init__.py
@@ -5,6 +5,19 @@ Storage.
 from .. import config
 from .. import strategy
 
+STORAGE_MAP = {
+    'sql': {
+        'has_storage_session': True
+    }
+}
+
+
+def has_storage_session(storage_strategy=None):
+    """Return boolean flagging the need for a storage session."""
+    return STORAGE_MAP[
+        storage_strategy or config.STORAGE_STRATEGY].get(
+            'has_storage_session', True)
+
 
 def get_storage_session(storage_strategy=None):
     """Return Schema class."""
