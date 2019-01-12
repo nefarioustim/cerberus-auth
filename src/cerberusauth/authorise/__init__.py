f8c2be796c0b2775e6a82e4147805670157d50e5
Tim Huegdon <tim@nefariousdesigns.co.uk>
2019-01-12 14:26:47 +0000

    feat(AuthoriseService): Add NewPermissionsCommand to AuthoriseService.

--- src/cerberusauth/authorise/__init__.py
+++ src/cerberusauth/authorise/__init__.py
@@ -4,15 +4,25 @@ Module containing core functionality for the authorisation application service.
 
 import logging
 
+from . import command
+
 
 def create_authorise_service(session=None, logger=None):
     """AuthoriseService factory."""
     logger = logger or logging.getLogger(__name__)
-    return AuthoriseService()
+    return AuthoriseService(
+        new_permissions_command=command.create_new_permissions_command(
+            session=session,
+            logger=logger
+        )
+    )
 
 
 class AuthoriseService(object):
     """
     An application service that controls all aspects of authorisation.
     """
-    pass
+
+    def __init__(self, new_permissions_command):
+        """Initialise RegisterService."""
+        self.new_permissions = new_permissions_command
--- tests/test_authorise_service.py
+++ tests/test_authorise_service.py
@@ -3,6 +3,7 @@ Tests for AuthoriseService.
 """
 
 from cerberusauth import authorise
+from cerberusauth.authorise import command
 
 
 def test_create_authorise_service():
@@ -11,3 +12,4 @@ def test_create_authorise_service():
 
     assert auth
     assert isinstance(auth, authorise.AuthoriseService)
+    assert isinstance(auth.new_permissions, command.NewPermissionsCommand)
