b34c870c5a5d271c6905bb93727577b9b9178b24
Tim Huegdon <tim@nefariousdesigns.co.uk>
2019-01-13 22:01:15 +0000

    test(e2e tests): Updated e2e tests to expect RegisteredUsers returned.

--- e2etests/test_e2e.py
+++ e2etests/test_e2e.py
@@ -19,18 +19,19 @@ def test_schema_creation(cerberus_fixture):
 def test_register_new_user(cerberus_fixture):
     """."""
     new_user_dict = {"email": "geoff@ting.com", "fullname": "Geoff Jeff"}
-    new_users = cerberus_fixture.registration.register_users(new_user_dict)
+    new_users = cerberus_fixture.register.register_users(new_user_dict)
 
     assert new_users
     assert isinstance(new_users, list)
 
     new_user = new_users[0]
 
-    assert new_user.email == new_user_dict["email"]
-    assert new_user.fullname == new_user_dict["fullname"]
-    assert new_user.password
-    assert isinstance(new_user.password, bytes)
-    assert len(new_user.password) > 25
-    assert new_user.has_temp_password
-    assert new_user.created
-    assert new_user.modified
+    user = new_user.user
+    assert user.email == new_user_dict["email"]
+    assert user.fullname == new_user_dict["fullname"]
+    assert user.password
+    assert isinstance(user.password, bytes)
+    assert len(user.password) > 25
+    assert user.has_temp_password
+    assert user.created
+    assert user.modified
